604b5dafdbd58491aba623987e9fb1740ea6c8a8
KyleB <kylebullanday1@gmail.com>
2024-03-06 04:49:58 -0600

    Add validations to Dog Model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,6 @@
 class Dog < ApplicationRecord
+  belongs_to :group
+  belongs_to :country
+  belongs_to :section
+  validates :name, :sound, :meme_phrase, :provisional, :url, :image, :pdf, :age, :size, :coat_length, presence: true
 end
