8f2fdc88f125ed686b296bf05f8ebc8b7a81d2ba
KyleB <kylebullanday1@gmail.com>
2024-03-06 04:55:00 -0600

    Add Dogs index action and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'dogs/index'
-  get 'dogs/show'
+  resources :dogs, only: [:index, :show]
+  root 'dogs#index'
+  #get 'dogs/index'
+  #get 'dogs/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
