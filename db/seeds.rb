f99bfbfd11633a20c8c969b02b66e1133a223d00
KyleB <kylebullanday1@gmail.com>
2024-03-06 04:52:09 -0600

    Seed Dogs with Faker gem and csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+require 'csv'
+
+Dog.destroy_all
+Country.destroy_all
+Group.destroy_all
+Section.destroy_all
+
+csv_file = Rails.root.join('db/dog_breed.csv')
+csv_data = File.read(csv_file)
+
+dogs = CSV.parse(csv_data, headers: true)
+
+dogs.each do |dog|
+
+  country = Country.find_or_create_by(name: dog['country'])
+  group = Group.find_or_create_by(name: dog['group'])
+  section = Section.find_or_create_by(name: dog['section'])
+
+  Dog.create(
+    name: dog['name'],
+    provisional: dog['provisional'],
+    url: dog['url'],
+    image: dog['image'],
+    pdf: dog['pdf'],
+    country: country,
+    group: group,
+    section: section,
+    sound: Faker::Creature::Dog.sound,
+    meme_phrase: Faker::Creature::Dog.meme_phrase,
+    age: Faker::Creature::Dog.age,
+    size: Faker::Creature::Dog.size,
+    coat_length: Faker::Creature::Dog.coat_length
+  )
+end
