9839de2513a826be59cf06cb9f15d4ee6b76cfaa
KyleB <kylebullanday1@gmail.com>
2024-03-08 00:01:35 -0600

    Add a ERB conditional in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,25 +23,29 @@ dogs = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
 end
 
 # Loop through CSV rows
-dogs.each do |dog|
-  country = Country.find_or_create_by(name: dog['country'])
-  group = Group.find_or_create_by(name: dog['group'])
-  section = Section.find_or_create_by(name: dog['section'])
-
-  Dog.create(
-    name: dog['name'],
-    url: dog['url'],
-    image: dog['image'],
-    pdf: dog['pdf'],
-    country: country,
-    group: group,
-    section: section,
-    sound: Faker::Creature::Dog.sound,
-    meme_phrase: Faker::Creature::Dog.meme_phrase,
-    age: Faker::Number.between(from: 6, to: 25),
-    size: Faker::Creature::Dog.size,
-    coat_length: Faker::Creature::Dog.coat_length
-  )
+dogs.each do |data|
+  country = Country.find_or_create_by(name: data['country'])
+  group = Group.find_or_create_by(name: data['group'])
+  section = Section.find_or_create_by(name: data['section'])
+  if country && country.valid? && group && group.valid?
+    dog = Dog.create(
+      name: data['name'],
+      url: data['url'],
+      image: data['image'],
+      pdf: data['pdf'],
+      country: country,
+      group: group,
+      section: section,
+      sound: Faker::Creature::Dog.sound,
+      meme_phrase: Faker::Creature::Dog.meme_phrase,
+      age: Faker::Number.between(from: 6, to: 25),
+      size: Faker::Creature::Dog.size,
+      coat_length: Faker::Creature::Dog.coat_length
+    )
+    puts "Invalid dog value: #{data["name"]}" unless dog.valid?
+  else
+    puts "Invalid or duplicate #{country} or #{group}"
+  end
 end
 
 puts "Created #{Group.count} groups"
